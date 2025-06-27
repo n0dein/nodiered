5bca73cf7421a4da536145e65b764950f8db57df
n0dein <donhowmat@gmail.com>
2025-06-26 22:02:47 -0400

    Update overworld.h

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -92,7 +92,7 @@
 #define OW_TIME_OF_DAY_ENCOUNTERS       FALSE         // If TRUE, will allow the user to define and use different encounter tables based on the time of day.
 #define OW_TIME_OF_DAY_DISABLE_FALLBACK FALSE        // If TRUE, if the encounter table for a specific map and time is empty, the area will have no encounters instead of falling back to the vanilla map and time.
 #define OW_TIME_OF_DAY_FALLBACK         TIME_MORNING // The time of day that encounter tables fall back to.
-#define OW_SEASONS                      TRUE         // Enables seasonal tilesets
+#define OW_SEASONS                      FALSE         // Enables seasonal tilesets
 #define OW_SEASON_ENCOUNTERS            FALSE
 #define OW_SEASON_DISABLE_FALLBACK      FALSE
 #define OW_SEASON_FALLBACK              SEASON_SPRING
